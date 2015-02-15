58d04cc9471fa9677c1821f70ea083c1e28e7039
hghowe <hghowe@gmail.com>
2015-02-15 17:08:30 -0600

    Two textfields working
    I have two textfields that are laid out correctly and interact with the
    keyboard.

--- MLTest2/ViewController.swift
+++ MLTest2/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder();
+        return true;
+    }
 
 }
 
